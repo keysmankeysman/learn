6647c4b669505e9ca74eeb5166f1084d6260309b
Dima <d.klychnikov@petrosoft.su>
2023-04-17 16:36:37 +0300

    some

---
+++ oneColorpicker/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import {ColorPicker, ColorPanel} from 'one-colorpicker'
+
+Vue.use(ColorPanel)
+Vue.use(ColorPicker)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ oneColorpicker/src/views/First.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>First</h2> 
+        <color-picker v-model="color" @change="changeColor($event)"></color-picker>
+    </div>
+</template>
+
+<script>
+import {ColorPicker} from 'one-colorpicker'
+
+export default {
+    name: 'first',
+    components: {
+      ColorPicker,
+    },
+    data() {
+        return {
+          color: '#eee' 
+        }
+    },
+    methods: {
+        changeColor(event) {
+            console.log('поменяли цвет')
+            console.log(event)
+        }
+    },
+
+}
+</script>
